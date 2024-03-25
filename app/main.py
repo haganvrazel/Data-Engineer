8544637188377a62745e9ce41df605ce21e4f9ef
haganvrazel <vrazel97@yahoo.com>
2024-03-25 16:12:19 -0500

    fixed issue where symbol was trying to be cast as an int

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def process_trades(input_file):
     with open(input_file, 'r', newline='') as csvfile:
         trade_processor = csv.reader(csvfile)
         for row in trade_processor:
-            timestamp, symbol, quantity, price = map(int, row[0].split(','))
+            timestamp, symbol, quantity, price = int(row[0]), row[1], int(row[2]), int(row[3])
             trade = (timestamp, quantity, price)
             trades_by_symbol[symbol].append(trade)
 
