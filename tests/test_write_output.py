bfa824481cd954ee07824bf0b9d77cc7b461277f
haganvrazel <vrazel97@yahoo.com>
2024-03-26 14:05:57 -0500

    chose to use tmp_path over tmpdir

--- tests/test_write_output.py
+++ tests/test_write_output.py
@@ -1,13 +1,11 @@
 import pytest
-import os
 from app.main import write_output
 import csv
-from io import StringIO
 
 
 @pytest.fixture
-def output_file(tmpdir):
-    file_path = os.path.join(tmpdir, 'output.csv')
+def output_file(tmp_path):
+    file_path = tmp_path / 'output.csv'
     print(f"Temporary file path: {file_path}")
     return file_path
 
@@ -18,6 +16,5 @@ def test_write_output_single_entry(output_file):
 
     # Reading temporary output file
     with open(output_file, 'r') as file:
-        reader = csv.reader(file)
-        row = next(reader)
+        row = next(csv.reader(file))
         assert row == ['aaa', '1000', '45', '25', '30']
